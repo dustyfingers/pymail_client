b88fe3f6261882ce91189c16568d604087a2688a
Louie <dustyfingers01257@gmail.com>
2022-10-08 22:12:00 -0600

    project stop

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import smtplib
+
+# need to find a service i can use smtp with to continue this tutorial
+# protonmail requires a paid account, which....idk about that
+server = smtplib.SMTP('mail.protonmail.ch', 25)
